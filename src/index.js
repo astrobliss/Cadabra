c3c6c91b1057bec527e5f6c68c65b4d5adfb2f2c
LBess <lbessell7@gmail.com>
2019-11-07 19:38:46 -0600

    Testing index.html

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import * as serviceWorker from './serviceWorker';
 //ReactDOM.render(<App />, document.getElementById('root'));
 
 ReactDOM.render(
-    <h1>This is my cool react app</h1>,
-    document.getElementById('root')
+    <h1>Cadabra</h1>,
+    document.getElementByID('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
