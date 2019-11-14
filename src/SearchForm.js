5872273fd23d2817e8d8d8ffbaf56e249c59ffbe
LBess <lbessell7@gmail.com>
2019-11-14 13:13:18 -0600

    Added logic for sorting by price

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -25,7 +25,8 @@ class SearchForm extends React.Component {
             value: '',          // user input in the search box
             site: 'all',        // site(s) to search
             min: '',            // min price
-            max: ''             // max price
+            max: '' ,           // max price
+            productSort: 'none'        // sorting
         };
 
         // API objects
@@ -33,9 +34,10 @@ class SearchForm extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleRadioChange = this.handleRadioChange.bind(this);
+        this.handleSiteChange = this.handleSiteChange.bind(this);
         this.handleMinChange = this.handleMinChange.bind(this);
         this.handleMaxChange = this.handleMaxChange.bind(this);
+        this.handleSortChange = this.handleSortChange.bind(this);
     }
 
     handleChange(event) {
@@ -43,7 +45,8 @@ class SearchForm extends React.Component {
             value: event.target.value,
             site: this.state.site,
             min: this.state.min,
-            max: this.state.max
+            max: this.state.max,
+            productSort: this.state.productSort
         });
     }
 
@@ -68,9 +71,17 @@ class SearchForm extends React.Component {
         }
         //
 
+        if (this.state.productSort.localeCompare("ascending") === 0) {      // sorting
+            console.log("Ascending sort");
+            results.sort(function(a, b) { return parseInt(a.price, 10) - parseInt(b.price, 10); });
+        } else if (this.state.productSort.localeCompare("descending") === 0) {
+            console.log("Descending sort");
+            results.sort(function(a, b) { return parseInt(b.price, 10) - parseInt(a.price, 10); });
+        }
+
         let items = [];     // contains JSX <li> objects
         for (let i = 0; i < results.length; i++) {
-            console.log(results[i].price);
+            //console.log(results[i].price);
             let flag = false;
 
             if (this.state.min.length > 0 && this.state.max.length > 0) {
@@ -129,13 +140,15 @@ class SearchForm extends React.Component {
         event.preventDefault();     // not sure what this does...
     }
 
-    handleRadioChange(event) {
+    handleSiteChange(event) {
         this.setState({
             value: this.state.value,
             site: event.target.value,
             min: this.state.min,
-            max: this.state.max
+            max: this.state.max,
+            productSort: this.state.productSort
         })
+        //console.log(event.target.value);
     }
 
     handleMinChange(event) {
@@ -143,7 +156,8 @@ class SearchForm extends React.Component {
             value: this.state.value,
             site: this.state.site,
             min: event.target.value,
-            max: this.state.max
+            max: this.state.max,
+            productSort: this.state.productSort
         })
     }
 
@@ -152,8 +166,20 @@ class SearchForm extends React.Component {
             value: this.state.value,
             site: this.state.site,
             min: this.state.min,
-            max: event.target.value
+            max: event.target.value,
+            productSort: this.state.productSort
         })
+    }
+
+    handleSortChange(event) {
+        this.setState({
+            value: this.state.value,
+            site: this.state.site,
+            min: this.state.min,
+            max: this.state.max,
+            productSort: event.target.value
+        })
+        //console.log(event.target.value);
     }
 
     render() {
@@ -167,26 +193,39 @@ class SearchForm extends React.Component {
 
                     <input type="submit" value="Search" /><br /><br />
 
-                    Price: <input type="number" name="min_price" placeholder="min" className ="PriceRange" title="Enter whole
+                    Price Range: <input type="number" name="min_price" placeholder="min" className ="PriceRange" title="Enter whole
                     number, no letters, no symbols" value={this.state.min} onChange={this.handleMinChange}/>
                     ~<input type="number" name="max_price" placeholder="max" className ="PriceRange" title="Enter whole
                     number, no letters, no symbols" value={this.state.max} onChange={this.handleMaxChange}/><br /><br />
 
-                    <label className="radio-inline">
-                        <input type="radio" name="site" value="all" onChange={this.handleRadioChange} defaultChecked />  All
+                    Sort Products by Price: <label className="radio-inline">
+                    <input type="radio" name="sort" value="none" onChange={this.handleSortChange} defaultChecked />  No Sort
                     </label>
 
                     <label className="radio-inline">
-                        <input type="radio" name="site" value="amazon" onChange={this.handleRadioChange} />  Amazon
+                        <input type="radio" name="sort" value="ascending" onChange={this.handleSortChange} />  Ascending
                     </label>
 
                     <label className="radio-inline">
-                        <input type="radio" name="site" value="ebay" onChange={this.handleRadioChange} />  Ebay
+                        <input type="radio" name="sort" value="descending" onChange={this.handleSortChange} />  Descending
+                    </label><br /><br />
+
+                    Marketplaces: <label className="radio-inline">
+                        <input type="radio" name="site" value="all" onChange={this.handleSiteChange} defaultChecked />  All
                     </label>
 
                     <label className="radio-inline">
-                        <input type="radio" name="site" value="craigslist" onChange={this.handleRadioChange} />  Craigslist
+                        <input type="radio" name="site" value="amazon" onChange={this.handleSiteChange} />  Amazon
                     </label>
+
+                    <label className="radio-inline">
+                        <input type="radio" name="site" value="ebay" onChange={this.handleSiteChange} />  Ebay
+                    </label>
+
+                    <label className="radio-inline">
+                        <input type="radio" name="site" value="craigslist" onChange={this.handleSiteChange} />  Craigslist
+                    </label><br /><br />
+
                 </form>
             </div>
         );
