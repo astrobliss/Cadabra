1a4c02ad0700e48ab29cbf5173fe9cb054fdb59f
LBess <lbessell7@gmail.com>
2019-11-14 16:28:50 -0600

    Advanced search button and min/max price error handling

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -18,26 +18,35 @@ const dummyData = [
     new SearchResult("waffle maker", "49", "ebay.com/really-good-waffle-maker")
 ];
 
+// Constants, maybe put this in class later?
+const defaultSite = 'all';
+const defaultSort = 'none';
+
 class SearchForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: '',          // user input in the search box
-            site: 'all',        // site(s) to search
-            min: '',            // min price
-            max: '' ,           // max price
-            productSort: 'none'        // sorting
+            value: '',                          // user input in the search box
+            site: defaultSite,                  // site(s) to search
+            min: '',                            // min price
+            max: '' ,                           // max price
+            productSort: defaultSort            // sorting
         };
 
         // API objects
         this.craigsList = new CraigsList();
 
+        // Misc State Vars
+        this.advancedSearch = false;
+
+        // Function Binding
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleSiteChange = this.handleSiteChange.bind(this);
         this.handleMinChange = this.handleMinChange.bind(this);
         this.handleMaxChange = this.handleMaxChange.bind(this);
         this.handleSortChange = this.handleSortChange.bind(this);
+        this.handleSearchChange = this.handleSearchChange.bind(this);
     }
 
     handleChange(event) {
@@ -51,6 +60,15 @@ class SearchForm extends React.Component {
     }
 
     handleSubmit(event) {       // triggers when form is submitted
+
+        // Error Handling
+        if (this.state.min.length > 0 && this.state.max.length > 0) {
+            if (parseInt(this.state.min, 10) > parseInt(this.state.max, 10)) {
+                alert("Minimum price can't be greater than maximum price");
+                return;
+            }
+        }
+
         let results = [];       // array of SearchResult objects
 
         // Call APIs, fill in results with the returned information
@@ -182,6 +200,27 @@ class SearchForm extends React.Component {
         //console.log(event.target.value);
     }
 
+    handleSearchChange(event) {
+        console.log("Handle search change trigger!");
+
+        if (this.advancedSearch === true) {     // need to implement a way to reset the radio buttons here...
+            this.advancedSearch = false;
+            document.getElementById("advanced_search").hidden = true;
+            this.setState({
+                value: this.state.value,
+                site: defaultSite,
+                min: '',
+                max: '',
+                productSort: defaultSort
+            });
+            document.getElementById("max").setAttribute("value", '')
+            document.getElementById("min").setAttribute("value", '')
+        } else {
+            this.advancedSearch = true;
+            document.getElementById("advanced_search").hidden = false;
+        }
+    }
+
     render() {
         return (
             <div className="SearchForm">
@@ -193,39 +232,46 @@ class SearchForm extends React.Component {
 
                     <input type="submit" value="Search" /><br /><br />
 
-                    Price Range: <input type="number" name="min_price" placeholder="min" className ="PriceRange" title="Enter whole
-                    number, no letters, no symbols" value={this.state.min} onChange={this.handleMinChange}/>
-                    ~<input type="number" name="max_price" placeholder="max" className ="PriceRange" title="Enter whole
-                    number, no letters, no symbols" value={this.state.max} onChange={this.handleMaxChange}/><br /><br />
-
-                    Sort Products by Price: <label className="radio-inline">
-                    <input type="radio" name="sort" value="none" onChange={this.handleSortChange} defaultChecked />  No Sort
-                    </label>
-
-                    <label className="radio-inline">
-                        <input type="radio" name="sort" value="ascending" onChange={this.handleSortChange} />  Ascending
-                    </label>
-
-                    <label className="radio-inline">
-                        <input type="radio" name="sort" value="descending" onChange={this.handleSortChange} />  Descending
+                    <label>
+                        <button type="button" onClick={this.handleSearchChange}>Advanced Search</button>
                     </label><br /><br />
 
-                    Marketplaces: <label className="radio-inline">
-                        <input type="radio" name="site" value="all" onChange={this.handleSiteChange} defaultChecked />  All
-                    </label>
+                    <div id="advanced_search" hidden>
 
-                    <label className="radio-inline">
-                        <input type="radio" name="site" value="amazon" onChange={this.handleSiteChange} />  Amazon
-                    </label>
+                        Price Range: <input id="min" type="number" name="min_price" placeholder="min" className ="PriceRange" title="Enter whole
+                        number, no letters, no symbols" value={this.state.min} onChange={this.handleMinChange}/>
+                        ~<input id="max" type="number" name="max_price" placeholder="max" className ="PriceRange" title="Enter whole
+                        number, no letters, no symbols" value={this.state.max} onChange={this.handleMaxChange}/><br /><br />
 
-                    <label className="radio-inline">
-                        <input type="radio" name="site" value="ebay" onChange={this.handleSiteChange} />  Ebay
-                    </label>
+                        Sort Products by Price: <label className="radio-inline">
+                        <input id="noSort" type="radio" name="sort" value="none" onChange={this.handleSortChange} defaultChecked />  No Sort
+                        </label>
 
-                    <label className="radio-inline">
-                        <input type="radio" name="site" value="craigslist" onChange={this.handleSiteChange} />  Craigslist
-                    </label><br /><br />
+                        <label className="radio-inline">
+                            <input id="ascending" type="radio" name="sort" value="ascending" onChange={this.handleSortChange} />  Ascending
+                        </label>
 
+                        <label className="radio-inline">
+                            <input id="descending" type="radio" name="sort" value="descending" onChange={this.handleSortChange} />  Descending
+                        </label><br /><br />
+
+                        Marketplaces: <label className="radio-inline">
+                            <input id="all" type="radio" name="site" value="all" onChange={this.handleSiteChange} defaultChecked />  All
+                        </label>
+
+                        <label className="radio-inline">
+                            <input id="amazon" type="radio" name="site" value="amazon" onChange={this.handleSiteChange} />  Amazon
+                        </label>
+
+                        <label className="radio-inline">
+                            <input id="ebay" type="radio" name="site" value="ebay" onChange={this.handleSiteChange} />  Ebay
+                        </label>
+
+                        <label className="radio-inline">
+                            <input id="craigslist" type="radio" name="site" value="craigslist" onChange={this.handleSiteChange} />  Craigslist
+                        </label><br /><br />
+
+                    </div>
                 </form>
             </div>
         );
