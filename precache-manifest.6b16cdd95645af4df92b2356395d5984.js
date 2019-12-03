17826718580e47299bade836e37bc07f0ec7435c
LBess <lbessell7@gmail.com>
2019-12-03 10:19:24 -0600

    Updates

---
+++ precache-manifest.6b16cdd95645af4df92b2356395d5984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fff4dda1d47ed22461e653686a27d9",
+    "url": "/lbessell/lbessell.github.io/index.html"
+  },
+  {
+    "revision": "7a2fd4cb86e18da7cbe6",
+    "url": "/lbessell/lbessell.github.io/static/css/main.96e7aed8.chunk.css"
+  },
+  {
+    "revision": "a37edb67b73cc135b3ff",
+    "url": "/lbessell/lbessell.github.io/static/js/2.46642d20.chunk.js"
+  },
+  {
+    "revision": "7a2fd4cb86e18da7cbe6",
+    "url": "/lbessell/lbessell.github.io/static/js/main.0383fbe8.chunk.js"
+  },
+  {
+    "revision": "898480b3ade0d06dc3d0",
+    "url": "/lbessell/lbessell.github.io/static/js/runtime-main.3875a893.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lbessell/lbessell.github.io/precache-manifest.55944ba1ad47bfd1439847b38f554c96.js"
+  "/lbessell/lbessell.github.io/precache-manifest.6b16cdd95645af4df92b2356395d5984.js"
 );
 
 self.addEventListener('message', (event) => {
